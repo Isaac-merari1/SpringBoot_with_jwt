eec62510316b88b5d3e1cd1b5ccbb3c0d63965c6
Isaac-merari1 <aduisaacmerari@gmail.com>
2023-09-18 20:50:08 -1200

    add jwt Autheication filter

---
+++ src/main/java/com/jwt/jwt/with/springboot/JwtWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.jwt.with.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithSpringbootApplication.class, args);
+	}
+
+}
